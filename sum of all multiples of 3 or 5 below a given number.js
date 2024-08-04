b9490a323e239cc62880c2ec0118cc1a2729f3c7
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-08-04 23:41:24 +0530

    sum of all multiples of 3 or 5 below a given number

---
+++ sum of all multiples of 3 or 5 below a given number.js
@@ -0,0 +1,14 @@
+function sumOfMultiples(n) {
+    let sum = 0
+    for(i=1;i<n;i++)
+    {
+        if(i%3 === 0 || i% 5 == 0)
+        {
+            sum+=i
+        }
+    }
+    return sum
+  }
+
+  console.log(sumOfMultiples(10))
+  
