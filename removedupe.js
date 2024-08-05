4090ac11a7ca8ce63fcfeb1eaae6e5611b36a60f
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-08-05 14:53:19 +0530

    removedupe

---
+++ removedupe.js
@@ -0,0 +1,21 @@
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(removeDuplicatesNI([1, 2, 21, 1, 1, 3, 41, 2, 4, 3, 1, 2]));
+
+function removeDuplicatesNI(arr) {
+  let newArr = [];
+  let dupe = {};
+
+  for (i = 0; i < arr.length; i++) {
+    if(!dupe[arr[i]])
+    {
+        dupe[arr[i]] = true
+        newArr.push(arr[i])
+    }
+  }
+  return newArr;
+}
+
+// console.log([1,1,13,3,2,2,3,2].includes(1))
