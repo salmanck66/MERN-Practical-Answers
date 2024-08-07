4f5a681423a05f32a36d51bbb9d5d8f489863e64
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-08-07 10:41:30 +0530

    update

--- arraypair.js
+++ arraypair.js
@@ -1,15 +1,17 @@
-const nums = [2, 11, 22, 15, 3, 7];
+const nums = [2, 11, 22, 15, 3, 6];
 const target = 9;
 
 function findPair(nums, target) {
-  let add = 0;
-  let pair = [Math.min(...nums)];
-  console.log(pair)
+  let pair = [];
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < target - (pair[i] ? pair[i] : 0) && nums[i]+pair[0]==target) {
-        pair.push(nums[i])
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        pair.push(nums[i], nums[j]);
+        return pair;
+      }
     }
   }
-  return pair
+  return pair;
 }
-console.log(findPair(nums, target)); // Output: [0, 1]
+
+console.log(findPair(nums, target)); // Output: [3, 6]
