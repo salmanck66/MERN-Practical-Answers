0539c58cd71c9c174dba71b5cbe0f89b5558f0be
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-08-09 17:45:19 +0530

    fd

--- randomGen.js
+++ randomGen.js
@@ -1,19 +1,20 @@
 ///input: adarsh
 //Eg81Br
 
-function randomGen(input)
-{
-    let gen = []
-    while(gen.length<input.split("").length/2)
-    {
-        rett =Math.floor( Math.random()*10)
-        if(gen.length<input.length)
-        {
-        if(rett<input.length )
-        gen.push(rett + input[rett] )
+function randomGen(input) {
+  let gen = [];
+  while (gen.length < input.split("").length / 2) {
+    rett = Math.floor(Math.random() * 10);
+    if (gen.length < input.length) {
+      if (rett < input.length)
+        if (rett < 5) {
+          gen.push(rett + input[rett].toLowerCase());
+        } else {
+          gen.push(input[rett].toUpperCase() + rett);
         }
     }
-    return gen.join("")
+  }
+  return gen.join("");
 }
 
-console.log(randomGen("Adarsh"))
+console.log(randomGen("Adarsh"));
